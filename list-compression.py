40dfd0619002df4112bff089e0f7f4f928927d36
Nipunraj k <nipunraj99@gmail.com>
2021-09-10 10:01:19 +0530

    transpose of matrix

--- list-compression.py
+++ list-compression.py
@@ -63,3 +63,20 @@ Example 5
 	
 	result = ['even' if item %2 ==0 else 'odd' for item in range(11) ]
 	print(result)
+	
+	
+Example 6
+	transposed = []
+	matrix = [[1, 2, 3, 4], [4, 5, 6, 8]]
+
+	for i in range(len(matrix[0])):	
+	transposed_row = []
+
+    	for row in matrix:
+        transposed_row.append(row[i])
+    	transposed.append(transposed_row)
+	print(transposed)	
+	
+	matrix = [[1, 2], [3,4], [5,6], [7,8]]
+	transpose = [[row[i] for row in matrix] for i in range(2)]
+	print (transpose)
